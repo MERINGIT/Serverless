d78c47b0cdc6cf8d0e2e51d79ac74f6ba6c3a40d
Library User <lib-user@atss-air.home>
2024-07-26 12:47:44 -0300

    updated lex bot

--- Frontend/dal-vacation-home/src/Features/Virtual-Assistant/index.js
+++ Frontend/dal-vacation-home/src/Features/Virtual-Assistant/index.js
@@ -4,6 +4,9 @@ import { useLocation } from "react-router-dom";
 export default function Chatbot() {
   const location = useLocation();
   useEffect(() => {
+    if(location.pathname === '/login' || location.pathname ==="/signup"){
+      return 0;
+    }
     // Load the chatbot script
     const script = document.createElement('script');
     script.src = 'https://d37ivrc17jbha3.cloudfront.net/lex-web-ui-loader.min.js';
@@ -28,10 +31,6 @@ export default function Chatbot() {
       document.body.removeChild(script);
     };
   }, []);
-
-  if(location.pathname === '/login' || location.pathname ==="/signup"){
-    return null;
-  }
 
   return <div id="chatbot-container"></div>;
 }
